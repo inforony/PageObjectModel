9a9f291178714ccfc2cdb37309673b0fc504713c
abull <inforony@yahoo.com>
2015-12-26 21:35:08 -0500

    just checkin pageobject

---
+++ src/pageOM/gmailLogingTest.java
@@ -0,0 +1,15 @@
+package pageOM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class gmailLogingTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://gmail.com");
+		loginpage lg = new loginpage(driver);
+		lg.txtbox.sendKeys("inforony0");
+	}
+
+}
---
+++ src/pageOM/loginpage.java
@@ -0,0 +1,21 @@
+package pageOM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class loginpage {
+	
+
+
+	public loginpage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+		
+		}
+	
+	
+	@FindBy(id="Email")
+	public WebElement txtbox;
+
+}
